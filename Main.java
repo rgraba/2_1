c14af5e7f7655bdcccfe252a4748890f888e5fda
rgraba <127319214+rgraba@users.noreply.github.com>
2023-03-22 13:38:19 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,14 @@
+class Main {
+  public static void main(String[] args) {
+    String str = "Hello World";
+    for(int i = 0; i < 3; i++)
+      {
+        for (int j = 0; j < str.length(); j++)
+        {
+        System.out.println(str.charAT(j));
+        }
+      }
+    System.out.println(" ");
+    }
+}
+  
